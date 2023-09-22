8908e61b75b71daf5738b0ac04bb399bb4ed5e4c
meraline <95317651+meraline@users.noreply.github.com>
2023-09-22 17:02:13 +0300

    app

--- first_app.py
+++ first_app.py
@@ -10,10 +10,26 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # Шаг 1
 #-----------------------------------------
-# st.title('Hello World! This is a Title')
+st.title('Hello World! This is a Title')
+#st.title('Привет Дениска ) Гы-гы-гы')
 
-# # Шаг 2
-##-----------------------------------------
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+# # # Шаг 2
+# ##-----------------------------------------
 
 # st.subheader('Here, a subheader')
 
@@ -21,8 +37,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # st.write('This is can be used for text and other features.')
 
-# # Шаг 3
-#-----------------------------------------
+# # # Шаг 3
+# #-----------------------------------------
 
 # df = sns.load_dataset('iris')
 
@@ -38,14 +54,14 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # st.write(df.head())
 
 
-# Шаг 4
-#-----------------------------------------
-image = Image.open('BDS.png')
-st.image(image)
+# # Шаг 4
+# # #-----------------------------------------
+# image = Image.open('BDS.png')
+# st.image(image)
 
 
-# Шаг 5
-#-----------------------------------------
+# # Шаг 5
+# #-----------------------------------------
 # latlong = {'London': {'lat': 51.509865,'lon': -0.118092},
 #            'Paris': {'lat': 48.864716, 'lon':2.349014},
 #            'Rome': {'lat': 41.902782,'lon': 12.496366},
@@ -60,19 +76,19 @@ st.image(image)
 # map_df = df[['lat', 'lon']]
 # st.map(map_df, zoom=1)
 
-# Шаг 6
-#-----------------------------------------
-st.radio('Choose your option', options=('Option 1', 'Option 2', 'Option 3'))
+# # Шаг 6
+# #-----------------------------------------
+# st.radio('Choose your option', options=('Option 1', 'Option 2', 'Option 3'))
 
 
-st.slider('<-- Slide to the sides -->', min_value=0, max_value=10, value=5, step=1)
+# st.slider('<-- Slide to the sides -->', min_value=0, max_value=10, value=5, step=1)
 
-st.text('Checkbox')
-st.multiselect('What are your favorite colors',
-            ['Green', 'Yellow', 'Red', 'Blue'],
-            ['Blue', 'Green'])
+# st.text('Checkbox')
+# st.multiselect('What are your favorite colors',
+#             ['Green', 'Yellow', 'Red', 'Blue'],
+#             ['Blue', 'Green'])
 
-st.selectbox('Select Box',options=('Option 1', 'Option 2', 'Option 3'))
+# st.selectbox('Select Box',options=('Option 1', 'Option 2', 'Option 3'))
 
-title = st.text_input('My App Text Input', 'Write Something...')
-st.write('You wrote:  ', title)
+# title = st.text_input('My App Text Input', 'Write Something...')
+# st.write('You wrote:  ', title)
